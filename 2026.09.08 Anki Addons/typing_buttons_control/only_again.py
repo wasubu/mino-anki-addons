b00b77dbbc67987106ad69585a3d3c5e3ed988bf
wasubu <delmarmendoza41@gmail.com>
2026-09-09 20:16:10 +0900

    seems like this doesn't have a difference for some reason

--- 2026.09.08 Anki Addons/typing_buttons_control/only_again.py
+++ 2026.09.08 Anki Addons/typing_buttons_control/only_again.py
@@ -1,21 +1,27 @@
 from aqt import gui_hooks
+from anki.cards import Card
 
-def _has_target_tag(card) -> bool:
+
+def _has_target_tag(card: Card) -> bool:
     if not card or not card.note():
         return False
+
     return card.note().has_tag("ONLY_AGAIN")
 
 
-def _force_only_again_button(buttons_tuple, reviewer, card):
+def _force_only_again_button(buttons_tuple, reviewer, card: Card):
+    """Restricts the UI to show only the 'Again' button."""
     if _has_target_tag(card):
         return ((1, "Again"),)
+
     return buttons_tuple
 
 
-def _force_again_ease(ease_tuple, reviewer, card):
+def _force_again_ease(ease_tuple, reviewer, card: Card):
     if _has_target_tag(card):
         cont, _ = ease_tuple
         return (cont, 1)
+
     return ease_tuple
 
 
