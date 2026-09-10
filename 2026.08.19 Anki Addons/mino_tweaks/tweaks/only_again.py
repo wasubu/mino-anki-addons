49bb56665c30640027647b4398bd6863f8f38c86
wasubu <delmarmendoza41@gmail.com>
2026-09-10 20:42:12 +0900

    I'm so happy, with this new method now rating the card with ONLY_AGAIN tag does actually rate the card without any weird imprefections. This is progress but I'm 20% done. I still have to do more. there is a problem right now. even if fail audiovisual plays good chime and then rate the card 'again'

--- 2026.08.19 Anki Addons/mino_tweaks/tweaks/only_again.py
+++ 2026.08.19 Anki Addons/mino_tweaks/tweaks/only_again.py
@@ -14,6 +14,10 @@ def _force_only_again_button(buttons_tuple, reviewer, card):
 
 
 def _force_again_ease(ease_tuple, reviewer, card):
+    # Allow Good to pass if we are in the bypass state
+    if getattr(reviewer, "_bypassing_only_again", False):
+        return ease_tuple
+
     if _has_target_tag(card):
         cont, _ = ease_tuple
         return (cont, 1)
@@ -27,26 +31,16 @@ def _handle_js_message(
         reviewer = getattr(mw, "reviewer", None)
         if reviewer and reviewer.card:
             card = reviewer.card
-            note = card.note()
 
-            # 1. Temporarily strip tag so _force_again_ease lets Good pass
-            had_tag = False
-            if note and note.has_tag("ONLY_AGAIN"):
-                had_tag = True
-                note.remove_tag("ONLY_AGAIN")
-                note.flush()
-
-            # 2. Determine 'Good' ease rating
             button_count = mw.col.sched.answerButtons(card)
             good_ease = 2 if button_count in (2, 3) else 3
 
-            # 3. Grade card as Good
-            reviewer._answerCard(good_ease)
-
-            # 4. Re-apply ONLY_AGAIN tag so it retains behavior on future reviews
-            if had_tag and note:
-                note.add_tag("ONLY_AGAIN")
-                note.flush()
+            # Set flag -> Answer card -> Clear flag
+            reviewer._bypassing_only_again = True
+            try:
+                reviewer._answerCard(good_ease)
+            finally:
+                reviewer._bypassing_only_again = False
 
         return (True, None)
 
@@ -55,8 +49,6 @@ def _handle_js_message(
 
 def setup():
     """Registers hooks for ONLY_AGAIN logic and JS webview messages."""
-    gui_hooks.reviewer_will_init_answer_buttons.append(
-        _force_only_again_button
-    )
+    gui_hooks.reviewer_will_init_answer_buttons.append(_force_only_again_button)
     gui_hooks.reviewer_will_answer_card.append(_force_again_ease)
     gui_hooks.webview_did_receive_js_message.append(_handle_js_message)
